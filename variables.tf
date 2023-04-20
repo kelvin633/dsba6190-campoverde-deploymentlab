662817206ed55ede8f500ffc79c9c67708bfd33f
kelvin633 <kcampove@uncc.edu>
2023-04-19 22:39:00 -0400

    Dep

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "camp"
+  default     = "campo"
 }
 
 variable "class_name" {
