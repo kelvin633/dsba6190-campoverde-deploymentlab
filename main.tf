a482bbdb1b311f52fc2478aab0b1e8bdcede2129
kelvin633 <kcampove@uncc.edu>
2023-04-19 22:09:12 -0400

    Deployment_Combined 2

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_storage_account" "st2" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = local.tags
+  tags                     = local.tags
 }
 
 
