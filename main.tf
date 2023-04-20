4989c15e5a6eb20be8c21a5c1eef9cf0ed3d44e0
kelvin633 <kcampove@uncc.edu>
2023-04-19 22:05:45 -0400

    Deployment_Combined

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "random_integer" "deployment_id_suffix" {
 
 // Resource Group
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg2"
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg"
   location = var.location
 
   tags = local.tags
@@ -33,12 +33,163 @@ resource "azurerm_resource_group" "rg2" {
 
 // Storage Account
 
-resource "azurerm_storage_account" "storage" {
+resource "azurerm_storage_account" "st" {
   name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
-  resource_group_name      = azurerm_resource_group.rg2.name
-  location                 = azurerm_resource_group.rg2.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
+  #is_hns_enabled =  true
   tags = local.tags
+}
+
+// Machine Learning 
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}insights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}kv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_storage_account" "st2" {
+  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st2"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = local.tags
+}
+
+
+resource "azurerm_machine_learning_workspace" "mlw" {
+  name                    = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}mlw"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.insights.id
+  key_vault_id            = azurerm_key_vault.kv.id
+  storage_account_id      = azurerm_storage_account.st2.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+// DB Cosmos 
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}db"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 0
+  }
+
+}
+
+//Firewall
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sub" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pub" {
+  name                = "testpip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "fire" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.sub.id
+    public_ip_address_id = azurerm_public_ip.pub.id
+  }
+}
+
+//Container app
+resource "azurerm_log_analytics_workspace" "laws" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}laws"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "capp" {
+  name                       = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}capp"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.laws.id
+}
+resource "azurerm_container_app" "ca" {
+  name                         = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}ca"
+  container_app_environment_id = azurerm_container_app_environment.capp.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
 }
